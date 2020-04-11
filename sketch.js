3f992aeb15d07c391f132fdeb966fb7f796be71f
KARNAMROOPESH <63301556+KARNAMROOPESH@users.noreply.github.com>
2020-04-11 15:40:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var mrect,frect;
+
+function setup() {
+  createCanvas(800,400);
+
+  mrect = createSprite(100, 100, 50, 50);
+  mrect.shapeColor = "red";
+  mrect.debug = true;
+
+  frect = createSprite(400,200,50,50);
+  frect.shapeColor = "red";
+  frect.debug = true;
+}
+
+function draw() {
+  background(0);
+  
+  mrect.x = mouseX;
+  mrect.y = mouseY;
+
+  if(mrect.x - frect.x < mrect.width/2 + frect.width/2 
+    && frect.x - mrect.x < mrect.width/2 + frect.width/2 
+    && mrect.y - frect.y < mrect.height/2 + frect.height/2 
+    && frect.y - mrect.y < mrect.height/2 + frect.height/2 ){
+   mrect.shapeColor = "green";
+   frect.shapeColor = "green";
+  }
+  else{
+    mrect.shapeColor = "red";
+    frect.shapeColor = "red";
+  }
+  drawSprites();
+}
